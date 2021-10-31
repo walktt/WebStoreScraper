6493dadde00e9dbb4d131a0837a72382e253282f
walktt <walktt@Mail.ru>
2021-10-31 17:32:22 +0300

    YM support added

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import time
 import schedule
 from datetime import datetime
-from scrapWithSelenium import scrapDNS
-from scrapWithSelenium import scrapWB
-from scrapWithSelenium import scrapYM
+from scrapWithSelenium import scrap
+
 print('running...')
 print(datetime.now())
-# scrapDNS('https://www.dns-shop.ru/catalog/17a89a0416404e77/materinskie-platy/?p=','dns-mb.csv')
-# scrapWB('https://www.wildberries.ru/catalog/elektronika/noutbuki-i-kompyutery/komplektuyushchie-dlya-pk?sort=popular&page=', 'wb-pc-parts.csv')
 
 
-schedule.every(1).hours.do(scrapDNS,'https://www.dns-shop.ru/catalog/17a89a0416404e77/materinskie-platy/?p=','dns-mb.csv')
-schedule.every(1).hours.do(scrapWB,'https://www.wildberries.ru/catalog/elektronika/noutbuki-i-kompyutery/komplektuyushchie-dlya-pk?sort=popular&page=', 'wb-pc-parts.csv')
-schedule.every(1).hours.do(scrapDNS,'https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/?f[66c]=9mkqp-2681-2680-rouuc-3lh5p-4bpxa&p=','dns-laptops.csv')
-schedule.every(1).hours.do(scrapYM,
-        'https://market.yandex.ru/catalog--noutbuki-v-anape/54544/list?cpa=0&hid=91013&how=aprice&glfilter=5085102%3A16880592&onstock=1&local-offers-first=0&page=',
-        'ym-laptops.csv')
+schedule.every(1).hours.do(scrap,'wb','https://www.wildberries.ru/catalog/elektronika/noutbuki-i-kompyutery/komplektuyushchie-dlya-pk?sort=priceup&priceU=110000%3B23998000&page=','wb-pcparts.csv',20)
+schedule.every(1).hours.do(scrap,'dns','https://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/?f[p3q]=b3ci&p=','dns-laptops.csv',1,50000)
+schedule.every(1).hours.do(scrap,'ym',
+            'https://market.yandex.ru/catalog--noutbuki/54544/list?hid=91013&how=aprice&glfilter=5085102%3A16880592&glfilter=5085119%3A18002970%2C19081250%2C17754601%2C17944473%2C16625219%2C17691566%2C16337183%2C22475370%2C22475390&onstock=1&page=1&local-offers-first=0',
+        'ym-laptops.csv',1,50000)
+# schedule.every(1).hours.do(scrap,'dns','https://www.dns-shop.ru/search/?q=playstation+5&category=17a8978216404e77&p=','dns-ps5.csv',1)
 while True:
     schedule.run_pending()
     time.sleep(1)
--- scrapWithSelenium.py
+++ scrapWithSelenium.py
@@ -1,3 +1,5 @@
+# https://stackoverflow.com/questions/16180428/can-selenium-webdriver-open-browser-windows-silently-in-the-background
+
 from selenium import webdriver
 import csv
 from datetime import date
@@ -5,18 +7,18 @@ from datetime import datetime
 import os
 import sys
 import json
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
+import telebot
+from inspect import currentframe
 
-
-# https://stackoverflow.com/questions/16180428/can-selenium-webdriver-open-browser-windows-silently-in-the-background
-
+bot = telebot.TeleBot('1844013883:AAEGQdYOdnVYqDk7ugpx9clrWZRJWI9CLkY')
 
 class SiteItem:
-    def __init__(self, id, name, price):
+    def __init__(self, id, name, price, image = '',discount=0 ):
         self.id = id
         self.name = name
         self.price = price
+        self.image=image
+        self.discount = discount
 
     def __str__(self):
         return str((self.id , ', ' , self.name , ', ' , self.price , ';'))
@@ -24,6 +26,9 @@ class SiteItem:
     def __repr__(self):
         return str((self.id , ', ' , self.name , ', ' , self.price , ';'))
 
+def get_linenumber():
+    cf = currentframe()
+    return cf.f_back.f_lineno
 
 def getItemsDNS(url):
     PATH = 'C:\Python\chromedriver.exe'
@@ -36,8 +41,14 @@ def getItemsDNS(url):
         itemId = item.get_attribute('data-code')
         itemName = item.find_element_by_class_name('catalog-product__name').text
         itemPrice = ''.join(filter(str.isdigit, item.find_element_by_class_name('product-buy__price').text))
+        itemDiscount=0
+        if (len(itemPrice)>=6):
+            itemPriceLen = int(len(itemPrice) / 2)
+            oldPrice = itemPrice[itemPriceLen:]
+            itemPrice = itemPrice[:itemPriceLen]
+            itemDiscount = (int(oldPrice)-int(itemPrice))/int(oldPrice)*100
         if len(itemPrice) > 1:
-            items.append(SiteItem(itemId, itemName, itemPrice))
+            items.append(SiteItem(itemId, itemName, itemPrice, discount=itemDiscount))
     driver.quit()
     return items
 
@@ -53,12 +64,13 @@ def getItemsWB(url):
             itemId = item.find_element_by_tag_name("div").get_attribute('id')[1:]
             itemName = item.find_element_by_class_name('goods-name').text + ' ' + item.find_element_by_class_name('brand-name').text
             itemPrice = ''.join(filter(str.isdigit, item.find_element_by_class_name('lower-price').text))
-            # print(itemId,' ',itemName,' ',itemPrice)
+            itemImage =  item.find_element_by_tag_name("img").get_attribute('src')
         except:
-            print ('error ', sys.exc_info()[0])
+            print ('error line ', get_linenumber(),' ', sys.exc_info()[0])
+            print(itemId, ' ', itemName, ' ', itemPrice)
         else:
             if len(itemPrice) > 1:
-                items.append(SiteItem(itemId, itemName, itemPrice))
+                items.append(SiteItem(itemId, itemName, itemPrice, itemImage))
     driver.quit()
     return items
 
@@ -71,22 +83,24 @@ def getItemsYM(url):
     items = []
     for item in search:
         try:
-            # itemId = item.find_element_by_tag_name("div").get_attribute('id')[1:]
-            # itemName = item.find_element_by_class_name('goods-name').text + ' ' + item.find_element_by_class_name('brand-name').text
-            # itemPrice = ''.join(filter(str.isdigit, item.find_element_by_class_name('lower-price').text))
             itemId = json.loads(item.get_attribute('data-zone-data'))["id"]
             itemName = item.find_element_by_tag_name('h3').text
             itemPrice = json.loads(item.get_attribute('data-zone-data'))["price"]
-            print(itemId,' ',itemName,' ',itemPrice)
+            itemDiscount=0
+            if ("oldPrice" in json.loads(item.get_attribute('data-zone-data'))):
+                itemOldPrice = json.loads(item.get_attribute('data-zone-data'))["oldPrice"]
+                itemDiscount = (int(itemOldPrice) - int(itemPrice)) / itemOldPrice * 100
+            # print(itemId,' ',itemName,' ',itemPrice)
         except:
-            print ('error ', sys.exc_info()[0])
+            print ('error ', get_linenumber(), ' ', sys.exc_info()[0])
+            print(itemId, ' ', itemName, ' ', itemPrice)
         else:
             if type(itemPrice)==int or len(itemPrice) > 1:
-                items.append(SiteItem(itemId, itemName, itemPrice))
+                items.append(SiteItem(itemId, itemName, itemPrice,discount=itemDiscount))
     driver.quit()
     return items
 
-def write_csv(data, filename):
+def write_csv(data, filename,goodPrice):
     newlines = 0
     if not os.path.isfile(filename):
         my_file = open(filename, "w+")
@@ -97,95 +111,76 @@ def write_csv(data, filename):
             writer = csv.writer(file)
             if os.stat(filename).st_size == 0:
                 writer.writerow(fields)
-                for item in data:
-                    writer.writerow([item.id, item.name, item.price, date.today(), datetime.now().strftime("%H:%M:%S")])
-    else:
-        with open(filename, 'r', encoding='utf8', newline='') as file:
-            reader = csv.reader(file, delimiter=',')
-            fileList = list(reader)
-            for item in data:
-                exists = False
-                for fileItem in fileList:
-                    if item.id == fileItem[0]:       # сюда вписать замену цены в существующих строках иначе будут ошибки
-                        try:
-                            exists = True
-                            if item.price == fileItem[2]:
-                                continue
-                            if (item.price<fileItem[2]) and round((int(fileItem[2])-int(item.price))/int(fileItem[2])*100)>5:
-                                print ('Цена вниз на ' , item.id , ' ' , item.name,', старая ', fileItem[2], ', новая ', item.price, ', падение на ', int(fileItem[2])-int(item.price), ' ',round((int(fileItem[2])-int(item.price))/int(fileItem[2])*100),'%')
-                            fileItem[2] = item.price
-                            fileItem[3] = date.today()
-                            fileItem[4] = datetime.now().strftime("%H:%M:%S")
-                            continue
-                        except:
-                            print('line 95 error - ' , fileItem)
-                            continue
-                if exists==False:
-                    fileList.append([item.id,item.name,item.price])
-                    print('New line: ', item)
-                    newlines+=1
+                # for item in data:
+                #     writer.writerow([item.id, item.name, item.price, date.today(), datetime.now().strftime("%H:%M:%S")])
 
-        with open(filename, 'w', encoding='utf8', newline='') as file:
-            # file.truncate(0)
-            writer = csv.writer(file)
-            writer.writerows(fileList)
+    with open(filename, 'r', encoding='utf8', newline='') as file:
+        reader = csv.reader(file, delimiter=',')
+        fileList = list(reader)
+        for item in data:
+            exists = False
+            for fileItem in fileList:
+                if item.id == fileItem[0]:       # сюда вписать замену цены в существующих строках иначе будут ошибки
+                    try:
+                        exists = True
+                        if item.price == fileItem[2]:
+                            continue
+                        if (item.price<fileItem[2]) and round((int(fileItem[2])-int(item.price))/int(fileItem[2])*100)>9:
+                            pricedown = ('Цена вниз на ' + item.id + ' ' + item.name+', старая '+ fileItem[2]+ ', новая '+ item.price+ ', падение на '+ str(int(fileItem[2])-int(item.price))+ ' '+str(round((int(fileItem[2])-int(item.price))/int(fileItem[2])*100))+'%')
+                            print(pricedown)
+                            bot.send_message('277180656', pricedown)
+                            if (item.image != ''):
+                                bot.send_photo('277180656', item.image)
+                        fileItem[2] = item.price
+                        fileItem[3] = date.today()
+                        fileItem[4] = datetime.now().strftime("%H:%M:%S")
+                        continue
+                    except:
+                        print('error line ', get_linenumber() , ' ',sys.exc_info()[0], ' ', fileItem)
+                        continue
+            if exists==False:
+                fileList.append([item.id,item.name,item.price, date.today(), datetime.now().strftime("%H:%M:%S")])
+                # print('New line: ', item)
+                newlines+=1
+            if (int(item.price)<goodPrice or item.discount > 20):
+                pricenew=('!!!!!!!!' + str(item.id) + ' ' + item.name+ ' цена ' + str(item.price))
+                bot.send_message('277180656', pricenew)
+
+    with open(filename, 'w', encoding='utf8', newline='') as file:
+        # file.truncate(0)
+        writer = csv.writer(file)
+        writer.writerows(fileList)
     print('data written to ', filename, ' new lines: ', newlines)
 
-def scrapDNS(link, filename):
-    # link = 'https://www.dns-shop.ru/catalog/17a89a0416404e77/materinskie-platy/?p='
+def scrap(site, link, filename,maxPages=5,goodPrice=0):
+    print('doing',site,filename)
     page = 1
     all_items = []
 
     while True:
-        items = getItemsDNS(link + str(page))
+        items=[]
+        if (site=='ym'):
+            items = getItemsYM(link + str(page))
+        if (site=='wb'):
+            items = getItemsWB(link + str(page))
+        if (site=='dns'):
+            items = getItemsDNS(link + str(page))
         if items:
             all_items.extend(items)
             page += 1
-            if page > 20:
-                break
-        else:
-            break
-
-    print('Total records: ' + str(len(all_items)))
-    write_csv(all_items, filename)
-
-def scrapWB(link, filename):
-    page = 1
-    all_items = []
-
-    while True:
-        items = getItemsWB(link + str(page))
-        if items:
-            all_items.extend(items)
-            page += 1
-            if page > 20:
+            if page > maxPages:
                 break
         else:
             break
 
     print(datetime.now().strftime("%H:%M:%S"), ' Total records: ' + str(len(all_items)))
-    write_csv(all_items, filename)
+    write_csv(all_items, filename,goodPrice)
 
-def scrapYM(link, filename):
-    page = 1
-    all_items = []
 
-    while True:
-        items = getItemsYM(link + str(page))
-        if items:
-            all_items.extend(items)
-            page += 1
-            if page > 21:
-                break
-        else:
-            break
-
-    print(datetime.now().strftime("%H:%M:%S"), ' Total records: ' + str(len(all_items)))
-    write_csv(all_items, filename)
-
-# if __name__ == '__main__':
-#     scrapYM(
-#         'https://market.yandex.ru/catalog--noutbuki-v-anape/54544/list?cpa=0&hid=91013&how=aprice&glfilter=5085102%3A16880592&onstock=1&local-offers-first=0&page=',
-#         'ym-laptops.csv')
-    # scrapDNS('https://www.dns-shop.ru/catalog/17a89a0416404e77/materinskie-platy/?p=', 'dns-mb-test1.csv')
-    # scrapWB('https://www.wildberries.ru/catalog/elektronika/noutbuki-i-kompyutery/komplektuyushchie-dlya-pk?sort=popular&page=1','test.csv')
+if __name__ == '__main__':
+    pass
+    # scrap('ym', 'https://market.yandex.ru/catalog--noutbuki-v-anape/54544/list?cpa=0&hid=91013&how=aprice&glfilter=5085102%3A16880592&onstock=1&local-offers-first=0&page=', 'ym-laptops.csv',1,50000)
+    # scrap('ym','https://market.yandex.ru/catalog--materinskie-platy-v-anape/55323/list?cpa=0&hid=91020&how=discount_p&glfilter=4923171%3A17781187&onstock=1&local-offers-first=0&page=','ym-mb1200.csv',1)
+    # scrap('dns','https://www.dns-shop.ru/catalog/17a89a0416404e77/materinskie-platy/?f[8lf]=evwg-2vyw1-rj64n&p=', 'dns-mb.csv',5)
+    # scrap('dns','https://www.dns-shop.ru/catalog/17a8ae4916404e77/televizory/?fr[p2]=50-100&p=','dns-tv.csv',1)
+    # scrap('wb','https://www.wildberries.ru/catalog/elektronika/noutbuki-i-kompyutery/komplektuyushchie-dlya-pk?sort=popular&page=','wb-pcparts.csv',1)
